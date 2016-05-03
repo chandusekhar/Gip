9a8c1de7063ebb8e9e8cdb56ea4bc90f3667bc12
Ben Roberts <Ben Roberts@DESKTOP-IOFB4TJ>
2016-05-04 00:41:38 +1000

    V51

--- Gip/Stock.cs
+++ Gip/Stock.cs
@@ -18,7 +18,43 @@ namespace Gip
         {
 
         }
-        
+
+        public int GetEMA(int period, DateTime EndDate)
+        {
+            int SMA;
+            List<double> clproverperiod = new List<double>();
+
+            TradingDay mod = History.OrderBy(x => Math.Abs( x.date.Ticks - EndDate.Ticks)).First();
+            int index = History.IndexOf(mod);
+
+
+            for (int i=0; i<period; i++)
+            {
+                clproverperiod.Add(History[i + index].AdjustedClose);
+            }
+
+            double total = clproverperiod.Sum();
+
+            double SMAResult  = total / period;
+            double Weight = (2 / (period + 1));
+
+            double numerator;
+            double denomn;
+
+            //for (int i = period-1; i >= 0; i--)
+            //{
+            //    numerator += ((1 - period) ^ i) * History[i + index].ClosingPrice;
+            //    denomn += 
+            //}
+            return index; //dummy
+            
+        }
+        public void SortDates()
+        {
+            History = History.OrderByDescending(x => x.date).ToList();
+        }
+
+
     }
 
     public class TradingDay
